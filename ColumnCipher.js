36e8c6e5ae6b2db18a16df2348f3db0a91edcc64
Aarsh532 <74576719+Aarsh532@users.noreply.github.com>
2024-02-29 18:09:54 -0500

    Update

--- ColumnCipher.js
+++ ColumnCipher.js
@@ -1,3 +1,15 @@
+var message; 
+var key;
+var blocksize;
+
 function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
+    blocksize = value;
+}
+
+function Encrypt(){
+    key = document.getElementById('KeyToEncrypt').value
+    message = document.getElementById('MessageTOEncrypt').value
+    
+    
 }
