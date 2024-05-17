2c4125be8275f278d5541ef9938dc05d878d5398
Aarsh532 <74576719+Aarsh532@users.noreply.github.com>
2024-05-16 20:01:05 -0400

    Update

--- ColumnCipher.js
+++ ColumnCipher.js
@@ -9,7 +9,21 @@ function rangeSlide(value) {
 
 function Encrypt(){
     key = document.getElementById('KeyToEncrypt').value
-    message = document.getElementById('MessageTOEncrypt').value
+    input = document.getElementById('MessageTOEncrypt').value
+    blocksize = document.getElementById('myRange').value
     
-    
+    fs.readFile('/encrypt.py', encrypt(input, key, blocksize))
+        enc
+   //encryptedSentence = runEncryption(input, key, blocksize)
+   console.log(encryptedSentence)
+}
+
+function runEncryption(input, key, blocksize){
+    var jqXHR = $.ajax({
+        type: "POST",
+        url: "/encrypt.py",
+        data: { param: input, key, blocksize},
+    });
+
+    return jqXHR.responseText;
 }
